e3c7df9324461b8c052855de3d964141b7dab8df
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2021-09-13 05:20:06 +0700

    update favicon with roket.id logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
